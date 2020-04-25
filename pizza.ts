10fae1eb6a0168b16e7eb3cf332c7080ea9adcbf
yahooserg <seltexinc@gmail.com>
2020-04-25 22:47:02 +0700

    Save order api tested

--- pizza.ts
+++ pizza.ts
@@ -4,12 +4,16 @@ import {MyNodeConfig} from '../serverconfig/mynodeconfig';
 const myNodeConfig = new MyNodeConfig();
 import { MySqlService } from './services/mysql.service';
 const mySqlService = new MySqlService();
+let bodyParser = require('body-parser');
 
 const app: Application = express();
 import * as http from 'http';
 
 const server = http.createServer(app);
 server.listen(myNodeConfig.serverPort);
+
+app.use(bodyParser.urlencoded({ extended: false },{limit: '5mb'}));
+app.use(bodyParser.json({limit: '5mb'}));
 
 app.use(function(req, res, next) {
   let allowedOrigins = myNodeConfig.allowedOrigins;
@@ -44,10 +48,15 @@ app.get('/api/getorder/:order', function(req, res) {
   });
 });
 
-app.put('/api/saveorder', function(req, res) {
-  mySqlService.saveOrder(req.body.order, (items) => {
-    res.send(items);
-  });
+app.post('/api/saveorder', function(req, res) {
+  let order = req.body.order;
+  for (let i = 0; i < order.cart.length; i += 1) {
+    order.cart[i].price = (order.cart[i].price/order.usdPerCurrency).toFixed(2);
+    order.cart[i].price = parseFloat(order.cart[i].price);
+  }
+  // mySqlService.saveOrder(req.body.order, (items) => {
+  //
+  // });
 });
 
 app.use('/:path', express.static(__dirname + '/static'));
