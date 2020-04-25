25976db5ceacc90c8be537cec4e54817e3d32301
yahooserg <seltexinc@gmail.com>
2020-04-25 19:27:09 +0700

    Config files location added

--- pizza.ts
+++ pizza.ts
@@ -1,8 +1,10 @@
 import * as express from 'express';
 import { Application } from 'express';
+import {MyNodeConfig} from '../serverconfig/mynodeconfig';
+const myNodeConfig = new MyNodeConfig();
 
 const app: Application = express();
 import * as http from 'http';
 
 const server = http.createServer(app);
-server.listen(3339);
+server.listen(myNodeConfig.serverPort);
