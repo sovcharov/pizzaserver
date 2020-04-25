cdd2e1fbee5165a574bc0aa03d0ca570f6f19d76
yahooserg <seltexinc@gmail.com>
2020-04-25 19:30:55 +0700

    first api added and tested

--- pizza.ts
+++ pizza.ts
@@ -8,3 +8,19 @@ import * as http from 'http';
 
 const server = http.createServer(app);
 server.listen(myNodeConfig.serverPort);
+
+app.use(function(req, res, next) {
+  let allowedOrigins = myNodeConfig.allowedOrigins;
+  let origin : string = String(req.headers.origin);
+  if (allowedOrigins.indexOf(origin) > -1) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  // res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
+  next();
+});
+
+app.get('/api/getmenu', function(req, res) {
+  console.log("api works");
+});
