fc48e5858cefbeda07b65bc808dc4329f3ab1631
yahooserg <seltexinc@gmail.com>
2020-04-25 20:12:29 +0700

    set MySQL funcs to get and put data

--- services/mysql.service.ts
+++ services/mysql.service.ts
@@ -33,4 +33,82 @@ export class MySqlService {
       });
     connection.end();
   }
+
+  getOrders(user, callback) {
+    let items = [];
+    let error: any = false;
+    let query = `call getOrders('${user}');`;
+
+    let connection = mysql.createConnection(mySqlConnection);
+    let request = connection.query(query);
+
+    request
+      .on('error', function(err) {
+        error = err;
+      })
+      .on('result', function(row) {
+        items[items.length] = row;
+      })
+      .on('end', function() {
+        if (error) {
+          callback({result: false, error: error});
+        } else {
+          items.splice(items.length - 1, 1);
+          callback({result: true, data: items});
+        };
+      });
+    connection.end();
+  }
+
+  getOrder(order, callback) {
+    let items = [];
+    let error: any = false;
+    let query = `call getOrder('${order}');`;
+
+    let connection = mysql.createConnection(mySqlConnection);
+    let request = connection.query(query);
+
+    request
+      .on('error', function(err) {
+        error = err;
+      })
+      .on('result', function(row) {
+        items[items.length] = row;
+      })
+      .on('end', function() {
+        if (error) {
+          callback({result: false, error: error});
+        } else {
+          items.splice(items.length - 1, 1);
+          callback({result: true, data: items});
+        };
+      });
+    connection.end();
+  }
+
+  saveOrder(order, callback) {
+    let items = [];
+    let error: any = false;
+    let query = `call saveOrder('${order}');`;
+
+    let connection = mysql.createConnection(mySqlConnection);
+    let request = connection.query(query);
+
+    request
+      .on('error', function(err) {
+        error = err;
+      })
+      .on('result', function(row) {
+        items[items.length] = row;
+      })
+      .on('end', function() {
+        if (error) {
+          callback({result: false, error: error});
+        } else {
+          items.splice(items.length - 1, 1);
+          callback({result: true, data: items});
+        };
+      });
+    connection.end();
+  }
 }
